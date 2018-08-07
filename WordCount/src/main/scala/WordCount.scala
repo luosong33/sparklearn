4ed59e2283383c72d2e053c7b9c337b6c7939464
luosong <luosong3345@gmail.com>
2018-08-07 19:53:02 +0800

    0807

--- WordCount/src/main/scala/WordCount.scala
+++ WordCount/src/main/scala/WordCount.scala
@@ -3,19 +3,22 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object WordCount {
 
+  // 个人用例
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf()
       .setMaster("local[2]")
-      .setAppName("WordCount_")
+      .setAppName("WordCount")
     val sc = new SparkContext(conf)
 
-    sc.textFile("/home/luosong/xxx.txt")
+//    sc.textFile("/home/luosong/xxx.txt")
+    sc.textFile("D:\\Download\\graphx\\*")
       .flatMap(line => line.split(" "))
       .map(word => (word, 1))
       .reduceByKey(_ + _)  // 关键代码三行
       .sortBy(_._2, false)
       .repartition(1)
-      .saveAsTextFile("/home/luosong/out")
+//      .saveAsTextFile("/home/luosong/out")
+      .saveAsTextFile("D:\\Download\\graphx\\out")
 
   }
 
--- WordCount/src/main/scala/WordCount_sql_guanfang.scala
+++ WordCount/src/main/scala/WordCount_sql_guanfang.scala
@@ -2,6 +2,7 @@ import org.apache.spark.sql.SparkSession
 
 object WordCount_sql_guanfang {
 
+  // 官方用例
   def main(args: Array[String]): Unit = {
 
     val spark = SparkSession
